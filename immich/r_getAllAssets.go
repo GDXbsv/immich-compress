209a2f2005d708be0560f57eee05365df36166ba
sergei.baikin <sergei.baikin@fotograf.de>
2025-11-03 13:46:33 +0100

    feat: implement pagination for asset retrieval with trashed item filtering
    - Add pagination support to GetAllAssets using NextPage tracking -
    Filter out trashed items (IsTrashed == false) - Use WaitGroup for
    concurrent page requests - Simplify debug configuration template with
    placeholder values

--- immich/r_getAllAssets.go
+++ immich/r_getAllAssets.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 func (c *ClientSimple) GetAllAssets() <-chan struct {
@@ -16,46 +18,67 @@ func (c *ClientSimple) GetAllAssets() <-chan struct {
 	}, c.parallel)
 	go func() {
 		defer close(ch)
-		resp, err := c.client.SearchAssets(c.ctx, SearchAssetsJSONRequestBody{})
-		if err != nil {
-			ch <- struct {
-				Asset AssetResponseDto
-				Err   error
-			}{Asset: AssetResponseDto{}, Err: fmt.Errorf("error getting assets: %w", err)}
+		pageNum := float32(1)
+		var respCurrent *http.Response
+		var respNext *http.Response
+		var err error
+		var wg sync.WaitGroup
+		for pageNum != 0 {
+			wg.Wait()
+			wg.Go(func() {
+				respNext, err = c.client.SearchAssets(c.ctx, SearchAssetsJSONRequestBody{
+					Page: &pageNum,
+				})
+			})
+			if respNext == nil {
+				wg.Wait()
+			}
+			respCurrent = respNext
 
-			return
-		}
-
-		if resp.StatusCode != http.StatusOK {
-			defer func() {
-				_ = resp.Body.Close()
-			}()
-			bodyBytes, _ := io.ReadAll(resp.Body)
-			ch <- struct {
-				Asset AssetResponseDto
-				Err   error
-			}{Asset: AssetResponseDto{}, Err: fmt.Errorf("bad status code: %s, body: %s", resp.Status, string(bodyBytes))}
-
-			return
-		}
-
-		parsedResp, err := ParseSearchAssetsResponse(resp)
-		if err != nil {
-			ch <- struct {
-				Asset AssetResponseDto
-				Err   error
-			}{Asset: AssetResponseDto{}, Err: fmt.Errorf("error parsing response: %w", err)}
-		}
-		for _, item := range parsedResp.JSON200.Assets.Items {
-			select {
-			case <-c.ctx.Done():
-				return
-			default:
+			if err != nil {
 				ch <- struct {
 					Asset AssetResponseDto
 					Err   error
-				}{Asset: item, Err: nil}
+				}{Asset: AssetResponseDto{}, Err: fmt.Errorf("error getting assets: %w", err)}
+
+				return
 			}
+
+			if respCurrent.StatusCode != http.StatusOK {
+				defer func() {
+					_ = respCurrent.Body.Close()
+				}()
+				bodyBytes, _ := io.ReadAll(respCurrent.Body)
+				ch <- struct {
+					Asset AssetResponseDto
+					Err   error
+				}{Asset: AssetResponseDto{}, Err: fmt.Errorf("bad status code: %s, body: %s", respCurrent.Status, string(bodyBytes))}
+
+				return
+			}
+
+			parsedResp, err := ParseSearchAssetsResponse(respCurrent)
+			if err != nil {
+				ch <- struct {
+					Asset AssetResponseDto
+					Err   error
+				}{Asset: AssetResponseDto{}, Err: fmt.Errorf("error parsing response: %w", err)}
+			}
+			for _, item := range parsedResp.JSON200.Assets.Items {
+				select {
+				case <-c.ctx.Done():
+					return
+				default:
+					if !item.IsTrashed {
+						ch <- struct {
+							Asset AssetResponseDto
+							Err   error
+						}{Asset: item, Err: nil}
+					}
+				}
+			}
+			nextPage, _ := strconv.ParseFloat(*parsedResp.JSON200.Assets.NextPage, 32)
+			pageNum = float32(nextPage)
 		}
 	}()
 
